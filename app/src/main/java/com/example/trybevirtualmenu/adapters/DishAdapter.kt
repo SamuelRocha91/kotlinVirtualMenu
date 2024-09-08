77f64f0ffb50aabed746d70192f679b864671a47
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-08 18:04:32 -0300

    feat: inicia criação do adapter e do holder

---
+++ app/src/main/java/com/example/trybevirtualmenu/adapters/DishAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.trybevirtualmenu.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class DishAdapter: Adapter<DishAdapter.DishViewHolder>() {
+
+    class DishViewHolder(view: View): ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DishViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DishViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
