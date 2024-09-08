0eaaeccf40bd6ad6ef165e4f9ef2c352767c8e9b
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-08 16:21:02 -0300

    feat: cria dataClass para representação de dados de pratos

---
+++ app/src/main/java/com/example/trybevirtualmenu/domain/dish/Dish.kt
@@ -0,0 +1,9 @@
+package com.example.trybevirtualmenu.domain.dish
+
+data class Dish(
+    val id: Int,
+    val name: String,
+    val photo: Int,
+    val description: String,
+    val price: Double
+)
