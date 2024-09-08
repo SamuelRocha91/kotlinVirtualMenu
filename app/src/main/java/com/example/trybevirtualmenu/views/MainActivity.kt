b13c1a033a0a73c37876c0e15881b73b8f9239e1
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-08 17:16:14 -0300

    feat: captura o recycleView e o vincula a um layoutManger linear

--- app/src/main/java/com/example/trybevirtualmenu/views/MainActivity.kt
+++ app/src/main/java/com/example/trybevirtualmenu/views/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.trybevirtualmenu.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.trybevirtualmenu.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val dishList: RecyclerView by lazy { findViewById(R.id.main_menu) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dishList.layoutManager = LinearLayoutManager(baseContext)
+
     }
 }
