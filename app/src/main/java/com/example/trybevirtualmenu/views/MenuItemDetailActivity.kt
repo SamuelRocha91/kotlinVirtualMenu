6c3bd70ced06df8fc37497bf7ffa7b621007bed0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-08 22:13:57 -0300

    feat: captura atributos de layoout da activity de detalhes de pratos

--- app/src/main/java/com/example/trybevirtualmenu/views/MenuItemDetailActivity.kt
+++ app/src/main/java/com/example/trybevirtualmenu/views/MenuItemDetailActivity.kt
@@ -1,10 +1,21 @@
 package com.example.trybevirtualmenu.views
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trybevirtualmenu.R
 
 class MenuItemDetailActivity : AppCompatActivity() {
+
+    private val logo: ImageView by lazy { findViewById(R.id.detail_logo) }
+    private val image_dish: ImageView by lazy { findViewById(R.id.detail_image) }
+    private val title: TextView by lazy { findViewById(R.id.detail_title) }
+    private val name: TextView by lazy { findViewById(R.id.detail_name) }
+    private val description: TextView by lazy { findViewById(R.id.detail_description) }
+    private val price: TextView by lazy { findViewById(R.id.detail_price) }
+    private val button: Button by lazy { findViewById(R.id.detail_back) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
