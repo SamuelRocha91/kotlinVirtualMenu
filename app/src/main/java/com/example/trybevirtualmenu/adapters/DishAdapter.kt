331f9b0b4c0a11e4735dd4f860df74f3d564125f
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-08 21:52:25 -0300

    feat: cria atributo de escuta de eventos e um setter do atributo lá no adapter

--- app/src/main/java/com/example/trybevirtualmenu/adapters/DishAdapter.kt
+++ app/src/main/java/com/example/trybevirtualmenu/adapters/DishAdapter.kt
@@ -9,9 +9,16 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.trybevirtualmenu.R
 import com.example.trybevirtualmenu.domain.dish.Dish
+import com.example.trybevirtualmenu.interfaces.DishItemListener
 import com.google.android.material.imageview.ShapeableImageView
 
 class DishAdapter(val dishes: List<Dish>): Adapter<DishAdapter.DishViewHolder>() {
+
+    private var dishEventListener: DishItemListener? = null
+
+    fun setDishListener(listener: DishItemListener) {
+        this.dishEventListener = listener
+    }
 
     class DishViewHolder(view: View): ViewHolder(view) {
         val name: TextView = view.findViewById(R.id.item_menu_name)
